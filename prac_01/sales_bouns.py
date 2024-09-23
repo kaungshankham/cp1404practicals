bf6c07fdd9a324dbd03b43306c9b28ab0204ed8f
Nang Kaung Shan Kham <nangkaungshankham@my.jcu.edu.au>
2024-09-23 17:22:11 +0800

    calculate sales bonus

---
+++ prac_01/sales_bouns.py
@@ -0,0 +1,23 @@
+"""
+Program to calculate and display a user's bonus based on sales.
+If sales are under $1,000, the user gets a 10% bonus.
+If sales are $1,000 or over, the bonus is 15%.
+"""
+
+BONUS_ON_SALES = 1000
+MINIMUM_DISCOUNT_RATE = 0.10
+MAXIMUM_DISCOUNT_RATE = 0.15
+
+sales = float(input("Enter sales: $"))
+while sales >= 0:
+    if sales < BONUS_ON_SALES:
+        bonus_rate = MINIMUM_DISCOUNT_RATE
+    else:
+        bonus_rate = MAXIMUM_DISCOUNT_RATE
+    bonus = sales * bonus_rate
+    print(f"The bonus is ${bonus}")
+    sales = float(input("Enter sales: $"))
+
+
+
+
