75b0c94dadbd3498296a2fe466fb26a8ee60350d
Nang Kaung Shan Kham <nangkaungshankham@my.jcu.edu.au>
2024-09-28 22:14:50 +0800

    implement password validation and star display

---
+++ prac_02/password_stars.py
@@ -0,0 +1,26 @@
+PASSWORD_LENGTH = 10
+
+
+def main():
+    """Get password from user and display stars."""
+    password = get_invalid_password()
+    display_stars(password)
+
+
+def get_invalid_password():
+    """Check the password meets the PASSWORD_LENGTH."""
+    password = get_password()
+    while len(password) < PASSWORD_LENGTH:
+        password = get_password()
+    return password
+
+
+def display_stars(password):
+    """display stars using password's length."""
+    print("*" * len(password))
+
+
+def get_password():
+    """Get password from user."""
+    password = input("Enter password: ")
+    return password
