7db1ca70b62abb6fb9108531fff7fa28d7f470d3
Nang Kaung Shan Kham <nangkaungshankham@my.jcu.edu.au>
2024-10-13 16:35:29 +0800

    Add list exercise program

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,22 @@
+"""Basic list operations"""
+
+numbers = []
+for i in range(5):
+    number = int(input("Number: "))
+    numbers.append(number)
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average of the numbers is {sum(numbers)/len(numbers)}")
+
+
+"""Woefully inadequate security checker"""
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Username: ".lower())
+if username in usernames:
+    result = "Access granted"
+else:
+    result = "Access denied"
+print(result)
