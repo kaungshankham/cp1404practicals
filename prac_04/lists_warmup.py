49b4dd2ec088f8ac27beb09ea3a2aae548ee1838
Nang Kaung Shan Kham <nangkaungshankham@my.jcu.edu.au>
2024-10-13 15:57:56 +0800

    Add list warmup program

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,22 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+"""
+numbers[0]
+numbers[-1]
+numbers[3]
+numbers[:-1]
+numbers[3:4]
+5 in numbers
+7 in numbers
+"3" in numbers
+numbers + [6, 5, 3]
+"""
+
+numbers[0] = "ten"
+print(numbers)
+
+numbers[-1] = 1
+print(numbers)
+
+print(numbers[2:])
+
+print(9 in numbers)
