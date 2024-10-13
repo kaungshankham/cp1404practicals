c37b27cff44e38e87e894a4c1407dc1de291ddaa
Nang Kaung Shan Kham <nangkaungshankham@my.jcu.edu.au>
2024-10-13 16:28:07 +0800

    Add subject reader program

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -9,21 +9,30 @@ FILENAME = "subject_data.txt"
 def main():
     data = load_data()
     print(data)
+    display_data_detail(data)
 
 
 def load_data():
+    datas = []
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        # print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        # print(parts)  # See if that worked
+        # print("----------")
+        datas.append(parts)
     input_file.close()
+    return datas
+
+
+def display_data_detail(datas):
+    for data in datas:
+        print(f"{data[0]} is taught by {data[1]:12} and has {data[-1]:3} students")
 
 
 main()
