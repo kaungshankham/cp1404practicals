14cdd47b2565e0f30934419ee321185de44bdc55
Nang Kaung Shan Kham <kham.lily01@gmail.com>
2024-10-21 21:50:26 +0800

    update word occurrences program

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,16 @@
+"""Word Occurrences"""
+word_to_count = {}
+text = input("Text: ")
+words = text.split(" ")
+# print(words)
+for word in words:
+    if word in word_to_count:
+        word_to_count[word] += 1
+    else:
+        word_to_count[word] = 1
+# print(word_to_count)
+words = list(word_to_count.keys())
+words.sort()
+maximum_word_length = max(len(word) for word in words)
+for word in words:
+    print(f"{word:{maximum_word_length+1}}: {word_to_count[word]}")
